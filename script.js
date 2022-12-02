2eac76a69f8677543350e1dc7235d52f466afa38
arthurmary <113183213+arthurmary@users.noreply.github.com>
2022-12-02 12:05:56 +0100

    page_prod

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// HOME 
+
 const OpenNav = document.querySelector(".icon1")
 const FermerNav = document.querySelector(".fermer")
 const Menu = document.querySelector(".menu")
@@ -15,3 +17,25 @@ FermerNav.addEventListener("click", () =>{
         Menu.classList.remove("monter")
     }
 })
+
+// PRODUIT
+
+var MainImg =document.getElementById('MainImg');
+var smallimg =document.getElementsByClassName('small-img');
+
+smallimg[0].onclick = function(){
+    MainImg.src =smallimg[0].src;
+}
+
+smallimg[1].onclick = function(){
+    MainImg.src =smallimg[1].src;
+}
+smallimg[2].onclick = function(){
+    MainImg.src =smallimg[2].src;
+}
+
+smallimg[3].onclick = function(){
+    MainImg.src =smallimg[3].src;
+}
+
+
